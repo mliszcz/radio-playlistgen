0741b28fdc9bc0e1fe4f6e5bc005591cdaaa230e
mliszcz <liszcz.michal@gmail.com>
2015-02-07 16:47:58 +0100

    checker tested against local servers

--- test/checker_test.exs
+++ test/checker_test.exs
@@ -3,14 +3,33 @@ defmodule CheckerTest do
 
 	use ExUnit.Case
 
+	defp serve(what, port) do
+		me = self
+		ref = make_ref
+		Task.async fn ->
+			{:ok, ss} = :gen_tcp.listen(port, [])
+			send me, {:ok, ref}
+			{:ok, s} = :gen_tcp.accept(ss)
+			:gen_tcp.recv(s, 0)
+			:gen_tcp.send(s, what)
+			:gen_tcp.close(s)
+			:gen_tcp.close(ss)
+		end
+		receive do
+			{:ok, ^ref} -> :ok
+		end
+	end
+
 	# TODO run mocked servers on localhost
 
 	test "check IceCast stream" do
-		assert Checker.check("http://31.192.216.5:8000/rmf_fm", 2000) == true
+		serve("HTTP/1.0 200 OK\r\nContent-Length: 0\r\n\r\n", 8123)
+		assert Checker.check("http://localhost:8123", 2000) == true
 	end
 
 	test "check SHOUTcast stream" do
-		assert Checker.check("http://stream3.polskieradio.pl:8900/", 2000) == true
+		serve("ICY 200 OK\r\nContent-Length: 0\r\n\r\n", 8124)
+		assert Checker.check("http://localhost:8124", 2000) == true
 	end
 
 	# test "check non-stream URL" do
@@ -18,6 +37,6 @@ defmodule CheckerTest do
 	# end
 
 	test "check invalid URL" do
-		assert Checker.check("http://some.site.that.does.not.exist:12345", 2000) == false
+		assert Checker.check("http://localhost:812345", 2000) == false
 	end
 end
